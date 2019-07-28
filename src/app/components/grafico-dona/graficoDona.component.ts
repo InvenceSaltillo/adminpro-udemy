b47c543ef254a5cb49a45dd03e6aa2ca0e4e56c2
Riojas <cesar.riojas@hotmail.com>
2019-07-27 23:32:06 -0500

    Input, output y Viewchild

---
+++ src/app/components/grafico-dona/graficoDona.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Label, MultiDataSet } from 'ng2-charts';
+import { ChartType } from 'chart.js';
+
+
+@Component({
+  selector: 'app-grafico-dona',
+  templateUrl: './graficoDona.component.html',
+  styles: []
+})
+export class GraficoDonaComponent implements OnInit {
+
+
+  // Doughnut
+  @Input() ChartLabels: Label[] = [];
+  @Input() ChartData: MultiDataSet = [];
+  @Input() ChartType: ChartType;
+
+  @Input() leyenda = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,18 +2,30 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from '../shared/shared.module';
 
+import { FormsModule } from '@angular/forms';
+
+import { PagesComponent } from './pages.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
-import { PagesComponent } from './pages.component';
 import { PAGES_ROUTES } from './pages.routes';
+
+// Temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
+// Graficas
+import { ChartsModule } from 'ng2-charts';
+import { GraficoDonaComponent } from '../components/grafico-dona/graficoDona.component';
 
 @NgModule({
   declarations: [
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
-    Graficas1Component
+    Graficas1Component,
+    IncrementadorComponent,
+    GraficoDonaComponent
   ],
   exports: [
     PagesComponent,
@@ -23,7 +35,9 @@ import { PAGES_ROUTES } from './pages.routes';
   ],
   imports: [
     SharedModule,
-    PAGES_ROUTES
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
   ]
 })
 
